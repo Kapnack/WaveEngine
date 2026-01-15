345768af01807a62479dd921e827f90e84221df0
Kapnack <unknowrealperson@gmail.com>
2026-01-15 00:19:13 -0300

    Create Singleton

---
+++ TSDV-WaveEngine/src/Singleton/Singleton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+template <typename T>
+class Singleton
+{
+public:
+
+    static T& Instance()
+    {
+        static T instance;
+        return instance;
+    }
+
+protected:
+
+    Singleton() {}
+};
+
