7378139a927742424e3cee73f9fafe6bc77fe452
Kapnack <unknowrealperson@gmail.com>
2026-01-15 00:30:42 -0300

    Create ServiceProvider.h

---
+++ TSDV-WaveEngine/src/ServiceProvider/ServiceProvider.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Service.h"
+
+#include <unordered_map>
+#include <typeindex>
+#include <type_traits>
+
+using namespace std;
+
+template<typename T>
+concept ServiceStandard = is_base_of_v<Service, T>;
+
+class ServiceProvider
+{
+private:
+    static inline unordered_map<type_index, Service*> services;
+
+public:
+    template<ServiceStandard T>
+    static void Register(T* service);
+
+    template<ServiceStandard T>
+    static T* Get();
+
+    template<ServiceStandard T>
+    static T* TryGet();
+
+    template<ServiceStandard T>
+    static void UnRegister();
+
+    static void Clear();
+};
+
+#include "ServiceProvider.tpp"
