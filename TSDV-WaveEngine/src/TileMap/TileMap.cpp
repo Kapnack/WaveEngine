72ef6466540d6e9bca859f609fe007ec2401cbe1
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:12:05 -0300

    Update TileMap.cpp

--- TSDV-WaveEngine/src/TileMap/TileMap.cpp
+++ TSDV-WaveEngine/src/TileMap/TileMap.cpp
@@ -1,3 +1,14 @@
+#include "TileMap.h"
+
+#include "FileReader/FileReader.h"
+#include "TextureImporter/TextureImporter.h"
+#include "ServiceProvider/ServiceProvider.h"
+#include "Window/Window.h"
+
+#include <iostream>
+
+static constexpr int EMPTY_TILE = -1;
+
 TileMap::TileMap(const string& mapFilePath, const string& texturePath, const Vector2& textureSize)
 {
     TextureImporter textureImporter;
