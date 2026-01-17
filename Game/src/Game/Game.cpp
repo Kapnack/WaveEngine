a0fe3b040b9411c1e8f89217ad2f762c4f5f0205
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:39:33 -0300

    Update Game.cpp

--- Game/src/Game/Game.cpp
+++ Game/src/Game/Game.cpp
@@ -19,130 +19,48 @@ Game::~Game()
 
 void Game::Init()
 {
+	const string json = "Sprites/map.json";
+	const string spriteSheet = "Sprites/spritesheet.png";
 
-	// ------- SAMUS -------
-	TextureImporter textureImport;
+	tileMap = new TileMap(json, spriteSheet, Vector2{ 128, 160 });
 
-	textureImport.LoadTexture("Sprites/Samus Aran Sprite Sheet.png");
+	TextureImporter textureImporter;
 
-	samus = new Sprite(textureImport.GetLoadedTexture(), Vector4(1, 1, 1, 1));
+	textureImporter.LoadTexture("Sprites/Samus Aran Sprite Sheet.png");
 
-	pos = Vector3(width / 2, height / 2, 0);
-	samus->SetPosition(pos);
+	sprite = new Sprite(textureImporter.GetLoadedTexture());
+	sprite->SetScale(Vector3{ (float)GetWindow()->GetWidth() / 2, (float)GetWindow()->GetHeight() / 2, 0 });
+	sprite->SetPosition(Vector3{ (float)GetWindow()->GetWidth() / 2, (float)GetWindow()->GetHeight() / 2, 0 });
 
-	scale = Vector3(width / 2, width / 2, 0);
-	samus->SetScale(scale * 0.4f);
-
-	Vector2 textureSize = Vector2(860, 762);
-	Vector2 frameArea = Vector2(68, 65);
-
-	idle = new Animation(Vector2(20, textureSize.y - 40), frameArea, textureSize, 2, 1);
-
-	frameArea = Vector2(80, 60);
-
-	walkingRight = new Animation(Vector2(20, textureSize.y - 215), frameArea, textureSize, 5, 1);
-
-	walkingLeft = new Animation(Vector2(20, textureSize.y - 292), frameArea, textureSize, 5, 1);
-
-	samus->SetAnimation(idle);
-
-	// ------- RED SQUARE -------
-
-	redSquare = new Square(Vector4(1, 0, 0, 1));
-	redSquare->SetScale(samus->GetScale());
-	redSquare->SetPosition(Vector3(samus->GetPos().x - redSquare->GetScale().x, samus->GetPos().y, samus->GetPos().z));
-
-	startingScale = redSquare->GetScale();
-	endScale = startingScale * 1.3f;
-
-	// ------- BLUE SQUARE -------
-
-	blueSquare = new Square(Vector4(0, 0, 1, 0.5f));
-	blueSquare->SetPosition(Vector3(width / 2, height / 2, 0));
-	blueSquare->SetScale(samus->GetScale() * 2);
+	otherThing = new Sprite(textureImporter.GetLoadedTexture());
+	otherThing->SetScale(Vector3{ (float)GetWindow()->GetWidth() / 2, (float)GetWindow()->GetHeight() / 2, 0 });
+	otherThing->SetPosition(Vector3{ 0,0,0 });
 }
 
 void Game::Update()
 {
-	float delta = GetDeltaTime();
+	if (GetInput()->IsKeyPressed(Keys::W))
+		sprite->Translate(Vector3::Up() * GetTime()->GetDeltaTime() * 100);
 
-	if (!collitionDetected)
-	{
-		if (GetInput()->IsKeyPressed(Keys::A) && samus)
-		{
-			samus->SetAnimation(walkingRight);
-			samus->Translate(Vector3::Right() * movementSpeed * delta);
-		}
-		else if (GetInput()->IsKeyPressed(Keys::D) && samus)
-		{
-			samus->SetAnimation(walkingLeft);
-			samus->Translate(Vector3::Left() * movementSpeed * delta);
-		}
-		else
-			samus->SetAnimation(idle);
-	}
-	else if (samus)
-	{
-		samus->SetPosition(Vector3(redSquare->GetPos().x + redSquare->GetScale().x, samus->GetPos().y, 0));
-		samus->SetAnimation(idle);
-	}
+	if (GetInput()->IsKeyPressed(Keys::S))
+		sprite->Translate(Vector3::Down() * GetTime()->GetDeltaTime() * 100);
 
-	if (GetInput()->IsKeyPressed(Keys::LEFT))
-		redSquare->Rotate(180 * delta);
-	else if (GetInput()->IsKeyPressed(Keys::RIGHT))
-		redSquare->Rotate(-180 * delta);
+	if (GetInput()->IsKeyPressed(Keys::A))
+		sprite->Translate(Vector3::Left() * GetTime()->GetDeltaTime() * 100);
 
-	if (GetInput()->IsKeyPressed(Keys::UP))
-		redSquare->Scale(Vector3(100 * delta, 100 * delta, 0));
-	else if (GetInput()->IsKeyPressed(Keys::DOWN))
-		redSquare->Scale(Vector3(-100 * delta, -100 * delta, 0));
+	if (GetInput()->IsKeyPressed(Keys::D))
+		sprite->Translate(Vector3::Right() * GetTime()->GetDeltaTime() * 100);
 
-	collitionDetected = CollisionManager::CheckCollision(samus, redSquare);
 
-	if (blueSquare->GetPos().x <= width - 100.0f && isGoingLeft == true)
-	{
-		blueSquare->Translate(250 * delta, 0);
-		blueSquare->SetRotation(0.0f);
+	tileMap->Draw();
 
-		if (blueSquare->GetPos().x >= width - 100.0f)
-		{
-			isGoingLeft = false;
-			isGoingRight = true;
-		}
-	}
-	else if (blueSquare->GetPos().x >= 100.0f && isGoingRight == true)
-	{
-		blueSquare->Translate(-250 * delta, 0);
-
-		blueSquare->SetRotation(180.0f);
-
-		if (blueSquare->GetPos().x <= 100.0f)
-		{
-			isGoingLeft = true;
-			isGoingRight = false;
-		}
-	}
-
-	if (samus)
-		samus->Update();
-
-	if (samus)
-		samus->Draw();
-
-	if (redSquare)
-		redSquare->Draw();
-
-	if (blueSquare)
-		blueSquare->Draw();
+	sprite->Draw();
+	otherThing->Draw();
 }
 
 void Game::Unload()
 {
-	delete samus;
-	delete idle;
-	delete walkingRight;
-	delete walkingLeft;
-
-	delete redSquare;
-	delete blueSquare;
+	delete tileMap;
+	delete sprite;
+	delete otherThing;
 }
