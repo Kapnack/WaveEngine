cae891d258b8bd54bff83eaf632c2cc223f3fb48
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:21:48 -0300

    Create Tile.h

---
+++ TSDV-WaveEngine/src/TileMap/Tile.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Entity/Entity2D/Sprite/Sprite.h"
+
+class Tile : public Sprite
+{
+private:
+
+	int spriteSheetID = -1;
+	unsigned int id = 0;
+	bool collide = false;
+
+public:
+
+	Tile();
+	Tile(unsigned int texture);
+	~Tile();
+
+	bool CanCollide();
+	void SetCollide(bool walkable);
+
+	void SetID(unsigned int id);
+	unsigned int GetID();
+
+	void SetSpriteSheetID(int id);
+	unsigned int GetSpriteSheetID();
+};
+
