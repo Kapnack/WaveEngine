804c735db5f942044baf95bf2ea28de50a1f8e86
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:22:20 -0300

    Update Tile.cpp

--- TSDV-WaveEngine/src/TileMap/Tile.cpp
+++ TSDV-WaveEngine/src/TileMap/Tile.cpp
@@ -13,3 +13,33 @@ Tile::Tile(unsigned int texture) : Sprite(texture)
 Tile::~Tile()
 {
 }
+
+void Tile::SetCollide(bool walkable)
+{
+	this->collide = walkable;
+}
+
+bool Tile::CanCollide()
+{
+	return collide;
+}
+
+void Tile::SetID(unsigned int id)
+{
+	this->id = id;
+}
+
+unsigned int Tile::GetID()
+{
+	return id;
+}
+
+void Tile::SetSpriteSheetID(int id)
+{
+	spriteSheetID = id;
+}
+
+unsigned int Tile::GetSpriteSheetID()
+{
+	return spriteSheetID;
+}
