b5b156fbc189cb274481b59c966cfd5bdf64bc19
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:28:05 -0300

    Update CollisionManager.cpp

--- TSDV-WaveEngine/src/CollisionManager/CollisionManager.cpp
+++ TSDV-WaveEngine/src/CollisionManager/CollisionManager.cpp
@@ -1,4 +1,4 @@
-#include "CollisionManager.h"
+﻿#include "CollisionManager.h"
 #include "Collider.h"
 
 bool CollisionManager::CheckCollision(Entity2D* anEntity, Entity2D* otherEntity)
@@ -9,8 +9,13 @@ bool CollisionManager::CheckCollision(Entity2D* anEntity, Entity2D* otherEntity)
     Collider entityCollider = anEntity->GetCollider();
     Collider otherCollider = otherEntity->GetCollider();
 
-	return entityCollider.x < otherCollider.x + otherCollider.width &&
+    return entityCollider.x < otherCollider.x + otherCollider.width &&
         entityCollider.x + entityCollider.width > otherCollider.x &&
         entityCollider.y < otherCollider.y + otherCollider.height &&
         entityCollider.y + entityCollider.height > otherCollider.y;
+}
+
+bool CollisionManager::CheckCollision(Entity2D* entity, TileMap* tileMap)
+{
+    return false;
 }
