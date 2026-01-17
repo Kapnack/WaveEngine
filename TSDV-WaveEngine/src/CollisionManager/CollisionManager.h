7322d02da44054535cdf1577a236cf32132a361b
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:27:26 -0300

    Update CollisionManager.h

--- TSDV-WaveEngine/src/CollisionManager/CollisionManager.h
+++ TSDV-WaveEngine/src/CollisionManager/CollisionManager.h
@@ -2,10 +2,14 @@
 
 #include "Export.h"
 #include "Entity/Entity2D/Entity2D.h"
+#include "TileMap/TileMap.h"
+
+#include "ServiceProvider/ServiceProvider.h"
 
 WAVEEXPORT class CollisionManager
 {
 public:
 	WAVEEXPORT static bool CheckCollision(Entity2D* anEntity, Entity2D* otherEntity);
+	WAVEEXPORT static bool CheckCollision(Entity2D* anEntity, TileMap* tileMap);
 };
 
