ff213a6e1613986f7c53c5e0c35fad253fe10921
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:45:24 -0300

    Update Tile.cpp

--- TSDV-WaveEngine/src/TileMap/Tile.cpp
+++ TSDV-WaveEngine/src/TileMap/Tile.cpp
@@ -2,12 +2,10 @@
 
 Tile::Tile() : Sprite(0)
 {
-	SetUVCordinates(Vector2{ 0,0 }, Vector2{ 1,1 }); // UV por defecto
 }
 
 Tile::Tile(unsigned int texture) : Sprite(texture)
 {
-	SetUVCordinates(Vector2{ 0,0 }, Vector2{ 1,1 }); // UV por defecto
 }
 
 Tile::~Tile()
