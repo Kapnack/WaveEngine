b092e6c119f790e3ee91492802bed9da6eb7caca
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:39:54 -0300

    Update Game.h

--- Game/src/Game/Game.h
+++ Game/src/Game/Game.h
@@ -1,38 +1,16 @@
 #pragma once
 
-#include "Entity/Entity2D/Shape/Shape.h"
-#include "Entity/Entity2D/Shape/Square/Square.h"
-#include "Entity/Entity2D/Shape/Triangle/Triangle.h"
-#include "Entity/Entity2D/Sprite/Sprite.h"
 #include "BaseGame/BaseGame.h"
+
+#include "TileMap/TileMap.h"
 
 class Game : public BaseGame
 {
 private:
 
-	static const int starParts = 2;
-
-	Vector3 pos;
-	Vector3 scale;
-	Sprite* samus;
-
-	Vector3 startingScale;
-	Vector3 endScale;
-	Square* redSquare;
-	Square* blueSquare;
-
-	Animation* idle;
-	Animation* walkingRight;
-	Animation* walkingLeft;
-
-	const float movementSpeed = 150;
-	const float rotate = -180.0f;
-
-	bool shouldScale;
-	bool collitionDetected;
-
-	bool isGoingLeft = true;
-	bool isGoingRight;
+	TileMap* tileMap;
+	Sprite* sprite;
+	Sprite* otherThing;
 
 protected:
 
