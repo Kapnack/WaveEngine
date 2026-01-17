2a7b7385b4e4a399ae104fcc8532d8811f59ab18
Kapnack <unknowrealperson@gmail.com>
2026-01-17 05:30:46 -0300

    Update Entity2D.cpp

--- TSDV-WaveEngine/src/Entity/Entity2D/Entity2D.cpp
+++ TSDV-WaveEngine/src/Entity/Entity2D/Entity2D.cpp
@@ -1,22 +1,23 @@
 #include "Entity2D.h"
 
-void Entity2D::UpdateCollider()
-{
-	collider =
-	{
-	   position.x,
-	   position.y,
-	   scale.x,
-	   scale.y
-	};
-}
-
 Entity2D::Entity2D() : Entity()
 {
+	UpdateCollider();
 }
 
 Entity2D::~Entity2D()
 {
+}
+
+void Entity2D::UpdateCollider()
+{
+	collider =
+	{
+		position.x,
+		position.y,
+		scale.x,
+		scale.y
+	};
 }
 
 Collider Entity2D::GetCollider()
@@ -24,7 +25,7 @@ Collider Entity2D::GetCollider()
 	return collider;
 }
 
-Vector3 Entity2D::GetPos()
+Vector3 Entity2D::GetPosition()
 {
 	return position;
 }
@@ -41,10 +42,10 @@ void Entity2D::SetPosition(Vector3 vector)
 
 void Entity2D::SetPosition(float x, float y)
 {
+	previousPosition = position;
+
 	position.x = x;
 	position.y = y;
-
-	previousPosition = position;
 
 	SetTRS();
 	UpdateCollider();
@@ -112,34 +113,27 @@ void Entity2D::Scale(float x, float y)
 void Entity2D::SetRotation(float angle)
 {
 	rotation = angle;
-
 	SetTRS();
 }
 
 void Entity2D::Rotate(float angle)
 {
 	rotation += angle;
-
 	SetTRS();
 }
 
 void Entity2D::SetColor(Vector4 color)
 {
 	for (int i = 0; i < vertexSize; i++)
-	{
 		vertex[i].SetColor(color);
-	}
 
 	GetRenderer()->UpdateBuffer(vertex, vertexSize, VBO);
 }
 
 void Entity2D::SetVertexColor(int index, Vector4 color)
 {
-	if (index < 0 || index > indexSize)
-	{
-		cout << "ERROR: Index is: " + to_string(index) + ". and it should be between 0 to " + to_string(indexSize) + ".";
+	if (index < 0 || index >= vertexSize)
 		return;
-	}
 
 	vertex[index].SetColor(color);
 	GetRenderer()->UpdateBuffer(vertex, vertexSize, VBO);
