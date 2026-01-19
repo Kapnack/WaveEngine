ea4f8dd7d2608b343eca7c737106e414550a29f3
Kapnack <unknowrealperson@gmail.com>
2026-01-19 05:41:52 -0300

    Update CollisionManager.cpp
    Implement TileMap, entity2D collision

--- TSDV-WaveEngine/src/CollisionManager/CollisionManager.cpp
+++ TSDV-WaveEngine/src/CollisionManager/CollisionManager.cpp
@@ -15,5 +15,48 @@ bool CollisionManager::CheckCollision(const Entity2D& anEntity, const Entity2D& 
 
 bool CollisionManager::CheckCollision(const Entity2D& entity, const TileMap& tileMap) const
 {
+    Collider c = entity.GetCollider();
+
+    float tileW = tileMap.GetTileWidth();
+    float tileH = tileMap.GetTileHeight();
+
+    int mapW = tileMap.GetMapWidth();
+    int mapH = tileMap.GetMapHeight();
+
+    float windowH = ServiceProvider::Instance().Get<Window>()->GetHeight();
+
+    float left = c.x;
+    float right = c.x + c.width;
+    float bottom = c.y;
+    float top = c.y + c.height;
+
+    int leftTile = int(floor(left / tileW));
+    int rightTile = int(floor((right - 0.001f) / tileW));
+
+    int topTile = int(floor((windowH - bottom) / tileH));
+    int bottomTile = int(floor((windowH - top) / tileH));
+
+    leftTile = clamp(leftTile, 0, mapW - 1);
+    rightTile = clamp(rightTile, 0, mapW - 1);
+    topTile = clamp(topTile, 0, mapH - 1);
+    bottomTile = clamp(bottomTile, 0, mapH - 1);
+
+    for (int row = bottomTile; row <= topTile; ++row)
+    {
+        for (int col = leftTile; col <= rightTile; ++col)
+        {
+            for (int layer = 0; layer < tileMap.GetLayerCount(); ++layer)
+            {
+                Tile* tile = tileMap.GetTileAt(layer, row, col);
+
+                if (!tile || !tile->CanCollide())
+                    continue;
+
+                if (CheckCollision(entity, *tile))
+                    return true;
+            }
+        }
+    }
+
     return false;
 }
