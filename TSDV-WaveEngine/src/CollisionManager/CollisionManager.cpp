acda95adcceaec97395b6f1ab7c9022399770a0b
Kapnack <unknowrealperson@gmail.com>
2026-01-19 16:13:46 -0300

    Update CollisionManager.cpp
    Turn variables into const

--- TSDV-WaveEngine/src/CollisionManager/CollisionManager.cpp
+++ TSDV-WaveEngine/src/CollisionManager/CollisionManager.cpp
@@ -13,50 +13,52 @@ bool CollisionManager::CheckCollision(const Entity2D& anEntity, const Entity2D& 
 		entityCollider.y + entityCollider.height > otherCollider.y;
 }
 
+#include "ServiceProvider/ServiceProvider.h"
+
 bool CollisionManager::CheckCollision(const Entity2D& entity, const TileMap& tileMap) const
 {
-    Collider c = entity.GetCollider();
+	Collider c = entity.GetCollider();
 
-    float tileW = tileMap.GetTileWidth();
-    float tileH = tileMap.GetTileHeight();
+	const float tileW = tileMap.GetTileWidth();
+	const float tileH = tileMap.GetTileHeight();
 
-    int mapW = tileMap.GetMapWidth();
-    int mapH = tileMap.GetMapHeight();
+	const int mapW = tileMap.GetMapWidth();
+	const int mapH = tileMap.GetMapHeight();
 
-    float windowH = tileMap.GetMapHeight() * tileH;
+	const float windowH = tileMap.GetMapHeight() * tileH;
 
-    float left = c.x;
-    float right = c.x + c.width;
-    float bottom = c.y;
-    float top = c.y + c.height;
+	const float left = c.x;
+	const float right = c.x + c.width;
+	const float bottom = c.y;
+	const float top = c.y + c.height;
 
-    int leftTile = int(floor(left / tileW));
-    int rightTile = int(floor((right - 0.001f) / tileW));
+	int leftTile = int(floor(left / tileW));
+	int rightTile = int(floor((right - 0.001f) / tileW));
 
-    int topTile = int(floor((windowH - bottom) / tileH));
-    int bottomTile = int(floor((windowH - top) / tileH));
+	int topTile = int(floor((windowH - bottom) / tileH));
+	int bottomTile = int(floor((windowH - top) / tileH));
 
-    leftTile = clamp(leftTile, 0, mapW - 1);
-    rightTile = clamp(rightTile, 0, mapW - 1);
-    topTile = clamp(topTile, 0, mapH - 1);
-    bottomTile = clamp(bottomTile, 0, mapH - 1);
+	leftTile = clamp(leftTile, 0, mapW - 1);
+	rightTile = clamp(rightTile, 0, mapW - 1);
+	topTile = clamp(topTile, 0, mapH - 1);
+	bottomTile = clamp(bottomTile, 0, mapH - 1);
 
-    for (int row = bottomTile; row <= topTile; ++row)
-    {
-        for (int col = leftTile; col <= rightTile; ++col)
-        {
-            for (int layer = 0; layer < tileMap.GetLayerCount(); ++layer)
-            {
-                Tile* tile = tileMap.GetTileAt(layer, row, col);
+	for (int row = bottomTile; row <= topTile; ++row)
+	{
+		for (int col = leftTile; col <= rightTile; ++col)
+		{
+			for (int layer = 0; layer < tileMap.GetLayerCount(); ++layer)
+			{
+				Tile* tile = tileMap.GetTileAt(layer, row, col);
 
-                if (!tile || !tile->CanCollide())
-                    continue;
+				if (!tile || !tile->CanCollide())
+					continue;
 
-                if (CheckCollision(entity, *tile))
-                    return true;
-            }
-        }
-    }
+				if (CheckCollision(entity, *tile))
+					return true;
+			}
+		}
+	}
 
-    return false;
+	return false;
 }
