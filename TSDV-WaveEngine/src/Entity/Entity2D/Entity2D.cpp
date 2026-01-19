d3ffdc750b37d3d23db339d8d506c79c8ec283b3
Kapnack <unknowrealperson@gmail.com>
2026-01-19 05:53:14 -0300

    Update Entity2D
    Deleted unwanted overrides to the functions

--- TSDV-WaveEngine/src/Entity/Entity2D/Entity2D.cpp
+++ TSDV-WaveEngine/src/Entity/Entity2D/Entity2D.cpp
@@ -9,6 +9,11 @@ Entity2D::~Entity2D()
 {
 }
 
+Collider Entity2D::GetCollider() const
+{
+	return collider;
+}
+
 void Entity2D::UpdateCollider()
 {
 	collider =
@@ -18,113 +23,4 @@ void Entity2D::UpdateCollider()
 		scale.x,
 		scale.y
 	};
-}
-
-Collider Entity2D::GetCollider()
-{
-	return collider;
-}
-
-Vector3 Entity2D::GetPosition()
-{
-	return position;
-}
-
-Vector3 Entity2D::GetScale()
-{
-	return scale;
-}
-
-void Entity2D::SetPosition(Vector3 vector)
-{
-	SetPosition(vector.x, vector.y);
-}
-
-void Entity2D::SetPosition(float x, float y)
-{
-	previousPosition = position;
-
-	position.x = x;
-	position.y = y;
-
-	SetTRS();
-	UpdateCollider();
-}
-
-float Entity2D::GetRotation()
-{
-	return rotation;
-}
-
-void Entity2D::Translate(Vector3 translation)
-{
-	Translate(translation.x, translation.y);
-}
-
-void Entity2D::Translate(float x, float y)
-{
-	previousPosition = position;
-
-	position.x += x;
-	position.y += y;
-
-	SetTRS();
-	UpdateCollider();
-}
-
-void Entity2D::SetScale(Vector3 vector)
-{
-	SetScale(vector.x, vector.y);
-}
-
-void Entity2D::SetScale(float x, float y)
-{
-	scale.x = x;
-	scale.y = y;
-
-	SetTRS();
-	UpdateCollider();
-}
-
-void Entity2D::Scale(Vector3 vector)
-{
-	Scale(vector.x, vector.y);
-}
-
-void Entity2D::Scale(float x, float y)
-{
-	scale.x += x;
-	scale.y += y;
-
-	SetTRS();
-	UpdateCollider();
-}
-
-void Entity2D::SetRotation(float angle)
-{
-	rotation = angle;
-	SetTRS();
-}
-
-void Entity2D::Rotate(float angle)
-{
-	rotation += angle;
-	SetTRS();
-}
-
-void Entity2D::SetColor(Vector4 color)
-{
-	for (int i = 0; i < vertexSize; i++)
-		vertex[i].SetColor(color);
-
-	GetRenderer()->UpdateBuffer(vertex, vertexSize, VBO);
-}
-
-void Entity2D::SetVertexColor(int index, Vector4 color)
-{
-	if (index < 0 || index >= vertexSize)
-		return;
-
-	vertex[index].SetColor(color);
-	GetRenderer()->UpdateBuffer(vertex, vertexSize, VBO);
 }
