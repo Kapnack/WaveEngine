9a498219d5f6b15a9bbd47d707cb623a12f60e0b
Kapnack <unknowrealperson@gmail.com>
2026-01-21 01:39:41 -0300

    Update CollisionManager.h
    Export CollisionManager constructor

--- TSDV-WaveEngine/src/CollisionManager/CollisionManager.h
+++ TSDV-WaveEngine/src/CollisionManager/CollisionManager.h
@@ -9,6 +9,10 @@
 WAVEEXPORT class CollisionManager : public Service
 {
 public:
+
+	WAVEEXPORT CollisionManager();
+	WAVEEXPORT ~CollisionManager();
+
 	WAVEEXPORT bool CheckCollision(const Entity2D& anEntity, const Entity2D& otherEntity) const noexcept;
 	WAVEEXPORT bool CheckCollision(const Entity2D& anEntity, const TileMap& tileMap) const;
 };
