4dfcb5a1bcab4ecdfc5196dc794474ee771f31f9
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-23 03:12:17 -0300

    Update ImGuiClass.cpp

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClass.cpp
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClass.cpp
@@ -11,11 +11,11 @@ Window* ImGuiClass::GetWindow()
 
 void ImGuiClass::Init()
 {
+	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	ImGui_ImplGlfw_InitForOpenGL(GetWindow()->GetGLFWindow(), true);
 	ImGui_ImplOpenGL3_Init("#version 330");
-	IMGUI_CHECKVERSION();
 	ImGui::StyleColorsDark();
 }
 
@@ -27,12 +27,29 @@ void ImGuiClass::Update()
 
 	ImGui::Begin("Epic Window");
 
+	string label;
+
 	for (vector<ImGuiClassData>::iterator it = dataToDraw.begin(); it != dataToDraw.end(); ++it)
 	{
-		ImGui::SliderFloat(it->label.c_str(), &it->vector->x, it->min, it->max);
+		if (it->position != nullptr)
+		{
+			label = to_string(it->id) + ": Pos. ";
+			ImGui::InputFloat3(label.c_str(), &it->position->x);
+		}
+
+		if (it->rotation != nullptr)
+		{
+			label = to_string(it->id) + ": Rot. ";
+			ImGui::InputFloat3(label.c_str(), &it->rotation->x);
+		}
+
+		if (it->textureID != nullptr)
+		{
+			ImGui::Image((ImTextureID)(intptr_t)it->textureID, ImVec2(256, 256));
+		}
 	}
 
-	ImGui::Text("Application avarag Frame rate", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 	ImGui::End();
 }
 
@@ -40,25 +57,6 @@ void ImGuiClass::Draw()
 {
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void ImGuiClass::Unload()
-{
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-}
-
-void ImGuiClass::CreateVec3Editor(const string& paramName, Vector3& vector, float min, float max)
-{
-	ImGuiClassData newData;
-
-	newData.label = paramName;
-	newData.vector = &vector;
-	newData.min = min;
-	newData.max = min;
-
-	dataToDraw.push_back(newData);
 }
 
 ImGuiClass::ImGuiClass() : Service()
@@ -69,4 +67,23 @@ ImGuiClass::ImGuiClass() : Service()
 ImGuiClass::~ImGuiClass()
 {
 	Unload();
+}
+
+void ImGuiClass::CreateEditor(const unsigned int& ID, Vector3& position, Vector3& rotation, unsigned int* textureID)
+{
+	ImGuiClassData newData;
+
+	newData.id = ID;
+	newData.position = &position;
+	newData.rotation = &rotation;
+	newData.textureID = textureID;
+
+	dataToDraw.push_back(newData);
+}
+
+void ImGuiClass::Unload()
+{
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 }
