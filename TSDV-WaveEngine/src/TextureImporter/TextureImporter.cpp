d815216ed0758aa23637d92af3f661bd18c9b988
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-23 03:10:26 -0300

    Update TextureImporter.cpp

--- TSDV-WaveEngine/src/TextureImporter/TextureImporter.cpp
+++ TSDV-WaveEngine/src/TextureImporter/TextureImporter.cpp
@@ -29,6 +29,8 @@ void TextureImporter::LoadTexture(string filePath)
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
