27443e49b41ea99ae037cf8a9f5bb0ecbe3196b7
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 14:58:43 -0300

    Update TextureManager.cpp
    Implement Delete Texture By Name Logic

--- TSDV-WaveEngine/src/TextureImporter/TextureManager.cpp
+++ TSDV-WaveEngine/src/TextureImporter/TextureManager.cpp
@@ -35,4 +35,18 @@ void TextureManager::DeleteTexture(const unsigned int& ID)
 	delete textures[ID];
 	textures.erase(ID);
 	glDeleteTextures(ID, &ID);
+void TextureManager::DeleteTexture(const string& name)
+{
+	unordered_map<unsigned int, Texture*>::iterator it = find_if(textures.begin(), textures.end(),
+		[&name](const auto& pair)
+		{
+			return pair.second && pair.second->GetName() == name;
+		});
+
+	if (it == textures.end())
+		return;
+
+	glDeleteTextures(it->second->textureID, &it->second->textureID);
+	delete it->second;
+	textures.erase(it);
 }
