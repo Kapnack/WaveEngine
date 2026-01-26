6644c58cb1d3c42cb41290347e2a717de1a8d6d2
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 01:57:47 -0300

    Update MaterialsImGui.h

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MaterialsImGui/MaterialsImGui.h
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MaterialsImGui/MaterialsImGui.h
@@ -2,16 +2,26 @@
 
 #include "ImGuiClass/ImGuiClassStates/ImGuiClassState.h"
 #include "Material/MaterialManager.h"
+#include "Material/MaterialFactory.h"
 
 class MaterialsImGui : public ImGuiClassState
 {
 private:
 
 	MaterialManager* GetMaterialManager();
+	MaterialFactory* GetMaterialFactory();
 
 	bool deleteByName = false;
+
+	bool createMaterial = false;
+
+	string newMaterialName;
+	string vertexShader;
+	string fragmentShader;
 	char materialName[255]{};
 	int materialID = 0;
+
+	const ImVec2 textBoxSize = ImVec2(500, 120);
 
 public:
 
