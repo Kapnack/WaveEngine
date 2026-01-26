f4d270a9f4a759de2fcbe6bcf7fa0e304495409a
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 01:57:22 -0300

    Update ImGuiClassState.cpp
    Implement ResizeCallBack Logic

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/ImGuiClassState.cpp
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/ImGuiClassState.cpp
@@ -1,5 +1,20 @@
 #include "ImGuiClassState.h"
 
+int ImGuiClassState::ResizeCallback(ImGuiInputTextCallbackData* data)
+{
+    {
+        if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
+        {
+            auto* str = static_cast<std::string*>(data->UserData);
+            str->resize(data->BufTextLen);
+            data->Buf = str->data();
+        }
+
+        return 0;
+    }
+
+}
+
 ImGuiClassState::ImGuiClassState()
 {
 }
