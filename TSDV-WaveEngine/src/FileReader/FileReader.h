143b1fa3bc59f7024d983e82c5bab21ab85987bd
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 02:37:20 -0300

    Update FileReader.h
    Make FileReader a State and Add cosntructor and destructor

--- TSDV-WaveEngine/src/FileReader/FileReader.h
+++ TSDV-WaveEngine/src/FileReader/FileReader.h
@@ -1,18 +1,29 @@
 #pragma once
 
 #include <string>
+#include "ServiceProvider/Service.h"
 #include "Export.h"
+
+class BaseGame;
+class ServiceProvider;
 
 using namespace std;
 
-WAVEEXPORT class FileReader
+WAVEEXPORT class FileReader : public Service
 {
 private:
 
 	string GetAbsoluteFilePath(const string& filePath) const;
 
+	FileReader();
+	~FileReader();
+
+	friend class BaseGame;
+	friend class ServiceProvider;
+
 public:
 
-	WAVEEXPORT string ReadFile(string filePath);
+	WAVEEXPORT string ReadFileAbsolutePath(const string& filePath);
+	WAVEEXPORT string ReadFile(const string& filePath);
 };
 
