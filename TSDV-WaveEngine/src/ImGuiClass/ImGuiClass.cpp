abefd9fe3489b60b832138e9b900279acd82b0eb
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 00:51:33 -0300

    Implement TextureImGui

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClass.cpp
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClass.cpp
@@ -16,8 +16,25 @@ ImGuiClass::~ImGuiClass()
 	Unload();
 }
 
+Window* ImGuiClass::GetWindow()
+{
+	return ServiceProvider::Instance().Get<Window>();
+}
+
+Input* ImGuiClass::GetInput()
+{
+	return ServiceProvider::Instance().Get<Input>();
+}
+ 
 void ImGuiClass::Init()
 {
+	showEntitiesState = new EntitiesImGui();
+	showTexturesState = new TexturesImGui();
+	showMaterialsState = new MaterialsImGui();
+	showMeshState = new MeshImGui();
+
+	state = showEntitiesState;
+
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
@@ -28,48 +45,14 @@ void ImGuiClass::Init()
 
 void ImGuiClass::Unload()
 {
-	for (vector<ImGuiClassData*>::iterator it = dataToDraw.begin(); it != dataToDraw.end(); ++it)
-		delete* it;
+	delete showEntitiesState;
+	delete showTexturesState;
+	delete showMaterialsState;
+	delete showMeshState;
 
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
-}
-
-void ImGuiClass::GoToPreviousState()
-{
-	if ((static_cast<ImGuiClassStates>(currentState - 1)) == ImGuiClassStates::Begin)
-		return;
-
-	--currentState;
-}
-
-void ImGuiClass::GoToNextState()
-{
-	if ((static_cast<ImGuiClassStates>(currentState + 1)) == ImGuiClassStates::End)
-		return;
-
-	++currentState;
-}
-
-Window* ImGuiClass::GetWindow()
-{
-	return ServiceProvider::Instance().Get<Window>();
-}
-
-TextureManager* ImGuiClass::GetTextureManager()
-{
-	return ServiceProvider::Instance().Get<TextureManager>();
-}
-
-EntityManager* ImGuiClass::GetEntityManager()
-{
-	return ServiceProvider::Instance().Get<EntityManager>();
-}
-
-MaterialManager* ImGuiClass::GetMaterialManager()
-{
-	return ServiceProvider::Instance().Get<MaterialManager>();
 }
 
 void ImGuiClass::Update()
@@ -81,79 +64,28 @@ void ImGuiClass::Update()
 	ImGui::Begin("WaveEngine Debug Window");
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-	if (ImGui::Button("Previous Page", ImVec2(100, 25)))
-		GoToPreviousState();
+	if (ImGui::Button("Entities", ImVec2(100, 25)))
+		state = showEntitiesState;
 
 	ImGui::SameLine();
 
-	if (ImGui::Button("Next Page", ImVec2(100, 25)))
-		GoToNextState();
+	if (ImGui::Button("Textures", ImVec2(100, 25)))
+		state = showTexturesState;
 
-	string text;
-	Material* material;
+	ImGui::SameLine();
 
-	switch (static_cast<ImGuiClassStates>(currentState))
-	{
-	case ImGuiClassStates::ShowEntities:
+	if (ImGui::Button("Materials", ImVec2(100, 25)))
+		state = showMaterialsState;
 
-		for (auto it : GetEntityManager()->GetEntities())
-		{
-			if (it.second == nullptr)
-				continue;
+	ImGui::SameLine();
 
-			text = "EntityID: " + to_string(it.second->ID);
+	if (ImGui::Button("Meshes", ImVec2(100, 25)))
+		state = showMeshState;
 
-			ImGui::Text(text.c_str());
+	ImGui::Separator();
 
-			text = "ID: " + to_string(it.second->ID) + ". Position. ";
-			if (ImGui::InputFloat3(text.c_str(), &it.second->position.x))
-				it.second->SetTRS();
-
-
-			text = "ID: " + to_string(it.second->ID) + ". Rotation.";
-			if (ImGui::InputFloat3(text.c_str(), &it.second->rotation.x))
-				it.second->SetTRS();
-
-			text = "ID: " + to_string(it.second->ID) + ". Scale.";
-			if (ImGui::InputFloat3(text.c_str(), &it.second->scale.x))
-				it.second->SetTRS();
-
-			ImGui::Separator();
-		}
-
-		break;
-
-	case ImGuiClassStates::ShowTextures:
-
-		for (auto it : GetTextureManager()->GetTextures())
-		{
-			if (it.second == nullptr)
-				continue;
-
-			ImGui::Image(it.second->GetTextureID(), ImVec2(it.second->GetWidth() / 3, it.second->GetHeight() / 3), ImVec2(0, 1), ImVec2(1, 0));
-			ImGui::Separator();
-		}
-
-		break;
-
-	case ImGuiClassStates::ShowMaterials:
-
-		for (auto it : GetMaterialManager()->GetMaterials())
-		{
-			if (it.second == nullptr)
-				continue;
-
-			text = "Name: " + it.second->GetName() + ". Material ID: " + to_string(it.second->GetProgram()) + ".";
-
-			ImGui::ColorEdit4(text.c_str(), &it.second->color.x);
-			ImGui::Separator();
-		}
-
-		break;
-
-	default:
-		break;
-	}
+	if (state)
+		state->Update();
 
 	ImGui::End();
 }
@@ -162,16 +94,4 @@ void ImGuiClass::Draw()
 {
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void ImGuiClass::CreateEditor(const unsigned int& ID, Vector3& position, Vector3& rotation, unsigned int* textureID)
-{
-	ImGuiClassData* newData = new ImGuiClassData();
-
-	newData->id = ID;
-	newData->position = &position;
-	newData->rotation = &rotation;
-	newData->textureID = textureID;
-
-	dataToDraw.push_back(newData);
 }
--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClass.h
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClass.h
@@ -2,69 +2,48 @@
 
 #include "ServiceProvider/Service.h"
 
-#include "ServiceProvider/ServiceProvider.h"
-#include "TextureImporter/TextureManager.h"
-#include "Material/MaterialManager.h"
-#include "TextureImporter/Texture.h"
-#include "Entity/EntityManager.h"
 #include "Window/Window.h"
+#include "Input/Input.h"
+#include "ImGuiClassStates/ImGuiClassState.h"
+#include "ImGuiClassStates/TexturesImGui/TexturesImGui.h"
+#include "ImGuiClassStates/EntitiesImGui/EntitiesImGui.h"
+#include "ImGuiClassStates/MaterialsImGui/MaterialsImGui.h"
+#include "ImGuiClassStates/MeshImGui/MeshImGui.h"
 #include "Vector3.h"
 #include "Vector4.h"
 
 #include <string>
-
 #include <vector>
 
 using namespace std;
 
 class BaseGame;
 
-struct ImGuiClassData
-{
-	unsigned int id;
-	Vector3* position = nullptr;
-	Vector3* rotation = nullptr;
-	unsigned int* textureID = nullptr;
-};
-
-enum class ImGuiClassStates
-{
-	Begin,
-	ShowEntities,
-	ShowTextures,
-	ShowMaterials,
-	End
-};
-
 class ImGuiClass : public Service
 {
 private:
 
-	vector<ImGuiClassData*> dataToDraw;
+	EntitiesImGui* showEntitiesState;
+	TexturesImGui* showTexturesState;
+	MaterialsImGui* showMaterialsState;
+	MeshImGui* showMeshState;
+
+	ImGuiClassState* state;
 
 	Window* GetWindow();
 
-	TextureManager* GetTextureManager();
-	EntityManager* GetEntityManager();
-	MaterialManager* GetMaterialManager();
+	Input* GetInput();
 
-	short currentState = (short)ImGuiClassStates::ShowEntities;
+	bool isActive = false;
 
 	void Init();
 	void Update();
 	void Draw();
 	void Unload();
 
-	void GoToPreviousState();
-	void GoToNextState();
-
 	ImGuiClass();
 	~ImGuiClass();
 
 	friend class BaseGame;
 	friend class ServiceProvider;
-
-public:
-
-	WAVEEXPORT void CreateEditor(const unsigned int& ID, Vector3& position, Vector3& rotation, unsigned int* textureID = nullptr);
 };
