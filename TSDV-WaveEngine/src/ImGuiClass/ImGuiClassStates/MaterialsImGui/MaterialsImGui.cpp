8c5c220766dcc7b594fe2dd45f42aa3535ebe92f
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 01:58:03 -0300

    Update MaterialsImGui.cpp
    Implement Shader Creator

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MaterialsImGui/MaterialsImGui.cpp
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MaterialsImGui/MaterialsImGui.cpp
@@ -18,8 +18,31 @@ MaterialManager* MaterialsImGui::GetMaterialManager()
 	return ServiceProvider::Instance().Get<MaterialManager>();
 }
 
+MaterialFactory* MaterialsImGui::GetMaterialFactory()
+{
+	return ServiceProvider::Instance().Get<MaterialFactory>();
+}
+
 void MaterialsImGui::Update()
 {
+	text = "Create Material";
+	ImGui::Text(text.c_str());
+
+	ImGui::InputText("Material Name", newMaterialName.data(), newMaterialName.capacity() + 1, ImGuiInputTextFlags_CallbackResize, ResizeCallback, &newMaterialName);
+
+	ImGui::InputTextMultiline("Vertex Shader",
+		vertexShader.data(), vertexShader.capacity() + 1,
+		textBoxSize, ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_CallbackResize, ResizeCallback, &vertexShader);
+
+	ImGui::InputTextMultiline("Fragment Shader",
+		fragmentShader.data(), fragmentShader.capacity() + 1,
+		textBoxSize, ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_CallbackResize, ResizeCallback, &fragmentShader);
+
+	if (ImGui::Button("Create Material"))
+		GetMaterialFactory()->CreateMaterial(newMaterialName, vertexShader, fragmentShader);
+
+	ImGui::Separator();
+
 	text = "Delete by Name";
 	ImGui::Checkbox(text.c_str(), &deleteByName);
 
