f21bdbc8b69cdf36803ce9cbe9d818a5e20ade29
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 15:30:13 -0300

    Update TexturesImGui.h
    Add Texture Delete variables and replace buf[255] for string fileTexurePath

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/TexturesImGui/TexturesImGui.h
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/TexturesImGui/TexturesImGui.h
@@ -5,6 +5,10 @@
 #include "TextureImporter/TextureImporter.h"
 #include "TextureImporter/Texture.h"
 
+#include <string>
+
+using namespace std;
+
 class TexturesImGui : public ImGuiClassState
 {
 private:
@@ -12,9 +16,12 @@ private:
 	TextureManager* GetTextureManager();
 	TextureImporter* GetTextureImporter();
 
-	char buf[255]{' '};
+	string fileTexurePath;
+	string textureName;
 
 	bool withAbsolutePath = false;
+	bool deleteByName = false;
+	int textureID = 0;
 
 public:
 
