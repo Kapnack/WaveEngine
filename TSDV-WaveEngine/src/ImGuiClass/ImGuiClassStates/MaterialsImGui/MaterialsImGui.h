4a450e30631b1eec428ff9d8ddefd04ee0bd4cd3
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-01-26 02:59:15 -0300

    Update MaterialsImGui.h
    Add File Reader and bools

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MaterialsImGui/MaterialsImGui.h
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MaterialsImGui/MaterialsImGui.h
@@ -3,6 +3,7 @@
 #include "ImGuiClass/ImGuiClassStates/ImGuiClassState.h"
 #include "Material/MaterialManager.h"
 #include "Material/MaterialFactory.h"
+#include "FileReader/FileReader.h"
 
 class MaterialsImGui : public ImGuiClassState
 {
@@ -10,14 +11,23 @@ private:
 
 	MaterialManager* GetMaterialManager();
 	MaterialFactory* GetMaterialFactory();
+	FileReader* GetFileReader();
 
 	bool deleteByName = false;
 
 	bool createMaterial = false;
+	bool importMaterial = false;
+
+	bool addAbsoluteToVertexPath = false;
+	bool addAbsoluteToFragmentPath = false;
 
 	string newMaterialName;
 	string vertexShader;
 	string fragmentShader;
+
+	string vertexShaderPath;
+	string fragmentShaderPath;
+
 	char materialName[255]{};
 	int materialID = 0;
 
