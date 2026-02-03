0450617f6a123bc7f7922f0796d1fd73808d60bc
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-02-03 13:07:41 -0300

    Update ImGuiClassState.cpp
    Replace auto for string

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/ImGuiClassState.cpp
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/ImGuiClassState.cpp
@@ -5,7 +5,7 @@ int ImGuiClassState::ResizeCallback(ImGuiInputTextCallbackData* data)
     {
         if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
         {
-            auto* str = static_cast<std::string*>(data->UserData);
+            string* str = static_cast<std::string*>(data->UserData);
             str->resize(data->BufTextLen);
             data->Buf = str->data();
         }
