110deb898766113d7ea58506a6c5b6a8c900d1e0
Kapnack <unknowrealperson@gmail.com>
2026-02-12 18:32:51 -0300

    Update Vector2
    Add more constructors, operators and Functions

--- TSDV-WaveEngine/src/Vector2.cpp
+++ TSDV-WaveEngine/src/Vector2.cpp
@@ -1,1 +1,77 @@
 #include "Vector2.h"
+
+#include "Vector3.h"
+
+Vector2::Vector2()
+{
+}
+
+Vector2::Vector2(const Vector2& other)
+{
+	Vector2(other.x, other.y);
+}
+
+Vector2::Vector2(const Vector3& vector3)
+{
+	x = vector3.x;
+	y = vector3.y;
+}
+
+Vector2::Vector2(const float& x, const float& y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+Vector2 operator*(const float& scalar, const Vector2 vector2)
+{
+	return Vector2(vector2.x * scalar, vector2.y * scalar);
+}
+
+void Vector2::operator*=(const float& scalar)
+{
+	x *= scalar;
+	y *= scalar;
+}
+
+void Vector2::operator=(const Vector2& other)
+{
+	x = other.x;
+	y = other.y;
+}
+
+void Vector2::operator=(const Vector3& vector3)
+{
+	x = vector3.x;
+	y = vector3.y;
+}
+
+Vector2 Vector2::operator/(const Vector2& other)
+{
+	return Vector2(x / other.x, y / other.y);
+}
+
+Vector2 Vector2::operator*(const float& scalar)
+{
+	return Vector2(x * scalar, y * scalar);
+}
+
+Vector2 Vector2::Up()
+{
+	return Vector2(0.0f, 1.0f);
+}
+
+Vector2 Vector2::Down()
+{
+	return Vector2(0.0f, -1.0f);
+}
+
+Vector2 Vector2::Right()
+{
+	return Vector2(1.0f, 0.0f);
+}
+
+Vector2 Vector2::Left()
+{
+	return Vector2(-1.0f, 0.0f);
+}
--- TSDV-WaveEngine/src/Vector2.h
+++ TSDV-WaveEngine/src/Vector2.h
@@ -2,18 +2,27 @@
 
 #include "Export.h"
 
+struct Vector3;
+
 WAVEEXPORT struct Vector2
 {
 	float x = 0.0f;
 	float y = 0.0f;
 
-	Vector2()
-	{
-	}
+	WAVEEXPORT	Vector2();
+	WAVEEXPORT	Vector2(const Vector2& other);
+	WAVEEXPORT	Vector2(const Vector3& vector3);
+	WAVEEXPORT	Vector2(const float& x, const float& y);
 
-	Vector2(float x, float y)
-	{
-		this->x = x;
-		this->y = y;
-	}
+	WAVEEXPORT	Vector2 operator/(const Vector2& other);
+	WAVEEXPORT	Vector2 operator*(const float& scalar);
+	WAVEEXPORT	friend Vector2 operator*(const float& scalar, const Vector2 vector2);
+	WAVEEXPORT	void operator*=(const float& scalar);
+	WAVEEXPORT	void operator=(const Vector2& other);
+	WAVEEXPORT	void operator=(const Vector3& vector3);
+
+	WAVEEXPORT	static Vector2 Up();
+	WAVEEXPORT	static Vector2 Down();
+	WAVEEXPORT	static Vector2 Right();
+	WAVEEXPORT	static Vector2 Left();
 };
