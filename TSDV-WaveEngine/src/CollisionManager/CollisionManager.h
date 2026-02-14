7c719588675a62d721941cdfa4cfb91b5e416dfb
Kapnack <unknowrealperson@gmail.com>
2026-02-14 14:17:36 -0300

    Update CollisionManager.h
    Make constructor and destructor private and add BaseGame and ServiceProvider as friend classes to prevent the from creating or destroying the a new or the current Service

--- TSDV-WaveEngine/src/CollisionManager/CollisionManager.h
+++ TSDV-WaveEngine/src/CollisionManager/CollisionManager.h
@@ -6,12 +6,21 @@
 #include "Entity/Entity2D/Entity2D.h"
 #include "TileMap/TileMap.h"
 
+class BaseGame;
+class ServiceProvider;
+
 WAVEEXPORT class CollisionManager : public Service
 {
+private:
+
+	CollisionManager();
+	~CollisionManager();
+
+	friend class BaseGame;
+	friend class ServiceProvider;
+
 public:
 
-	WAVEEXPORT CollisionManager();
-	WAVEEXPORT ~CollisionManager();
 
 	WAVEEXPORT bool CheckCollision(const unsigned int& anEntity, const unsigned int& otherEntity) const;
 	WAVEEXPORT bool CheckCollision(const Entity2D* anEntity, const TileMap& tileMap) const;
