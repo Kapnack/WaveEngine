3320007373a97727277f8e4726430ea95ea00fb1
Kapnack <unknowrealperson@gmail.com>
2026-02-14 23:35:50 -0300

    Update EntityController.cpp
    Delete pointer and use TryGet and Get

--- TSDV-WaveEngine/src/Entity/Controller/EntityController/EntityController.cpp
+++ TSDV-WaveEngine/src/Entity/Controller/EntityController/EntityController.cpp
@@ -14,25 +14,21 @@ EntityController::~EntityController()
 
 void EntityController::Update(const float& deltaTime)
 {
-	Entity* entity;
-
 	for (vector<unsigned int>::iterator it = entitiesIDs.begin(); it != entitiesIDs.end(); ++it)
 	{
-		entity = GetEntityManager()->Get<Entity>(*it);
-
-		if (entity == nullptr)
+		if (!GetEntityManager()->TryGet(*it))
 			continue;
 
 		if (GetInput()->IsKeyPressed(Keys::W))
-			entity->Translate(Vector3::Up() * deltaTime * 100);
+			GetEntityManager()->Get(*it)->Translate(Vector3::Up() * deltaTime * 100);
 
 		if (GetInput()->IsKeyPressed(Keys::S))
-			entity->Translate(Vector3::Down() * deltaTime * 100);
+			GetEntityManager()->Get(*it)->Translate(Vector3::Down() * deltaTime * 100);
 
 		if (GetInput()->IsKeyPressed(Keys::A))
-			entity->Translate(Vector3::Left() * deltaTime * 100);
+			GetEntityManager()->Get(*it)->Translate(Vector3::Left() * deltaTime * 100);
 
 		if (GetInput()->IsKeyPressed(Keys::D))
-			entity->Translate(Vector3::Right() * deltaTime * 100);
+			GetEntityManager()->Get(*it)->Translate(Vector3::Right() * deltaTime * 100);
 	}
 }
