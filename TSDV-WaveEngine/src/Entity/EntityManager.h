3be60c8ecaef03504c00d899b46da67d62f43589
Kapnack <unknowrealperson@gmail.com>
2026-02-18 00:19:11 -0300

    Update EntityManager.h
    Rename entityByLayer by drawableByLayer

--- TSDV-WaveEngine/src/Entity/EntityManager.h
+++ TSDV-WaveEngine/src/Entity/EntityManager.h
@@ -30,7 +30,7 @@ private:
 
 	unordered_map<type_index, vector<unsigned int>> entitiesIDByType;
 
-	map<int, list<unsigned int>> entityByLayer;
+	map<int, list<unsigned int>> drawableByLayer;
 
 	template<EntityManagerStandar T>
 	void SaveEntity(const unsigned int& ID, T* entity);
