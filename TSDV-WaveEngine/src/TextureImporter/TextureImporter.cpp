23495f314bbe7a1758f371cea153c107a7480d44
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-02-24 01:59:55 -0300

    Update TextureImporter.cpp
    Replace GL_LINEAR_MIPMAP_LINEAR for GL_NEAREST_MIPMAP_NEAREST

--- TSDV-WaveEngine/src/TextureImporter/TextureImporter.cpp
+++ TSDV-WaveEngine/src/TextureImporter/TextureImporter.cpp
@@ -53,7 +53,7 @@ unsigned int TextureImporter::LoadTexture(string filePath)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
