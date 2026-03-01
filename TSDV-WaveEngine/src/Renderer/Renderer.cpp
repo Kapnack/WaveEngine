ebd92da74eb937f69f3247d0f8cd2ec957a364ab
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-01 18:18:02 -0300

    Update Renderer.cpp
    Fix textureID attribute

--- TSDV-WaveEngine/src/Renderer/Renderer.cpp
+++ TSDV-WaveEngine/src/Renderer/Renderer.cpp
@@ -117,8 +117,8 @@ void Renderer::CreateBuffersSprite(const VertexData* vertex, const int& vertexSi
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, textureCordinates));
 	glEnableVertexAttribArray(2);
 
-	glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, textureCordinates));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, textureID));
+	glEnableVertexAttribArray(3);
 
 	glBindVertexArray(0);
 }
