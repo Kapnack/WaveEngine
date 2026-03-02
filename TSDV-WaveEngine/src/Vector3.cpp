be1fc8cd3a79196797efee5c8d033c7b347ce055
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-02 20:59:00 -0300

    Update Vector3.cpp

--- TSDV-WaveEngine/src/Vector3.cpp
+++ TSDV-WaveEngine/src/Vector3.cpp
@@ -30,6 +30,11 @@ Vector3 Vector3::operator/(const Vector3& other)
 	return Vector3(x / other.x, y / other.y, z / other.z);
 }
 
+Vector3 Vector3::operator+(const Vector3& other)
+{
+	return Vector3(x + other.x, y + other.y, z + other.z);
+}
+
 void Vector3::operator+=(const Vector3& other)
 {
 	x += other.x;
