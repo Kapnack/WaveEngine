de5b57b610b98ff840c857d6521706a392728616
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-05 08:38:11 -0300

    Improve Vector3
    Add new operators and functions to improve the use of the Vector3 class

--- TSDV-WaveEngine/src/Vector3.h
+++ TSDV-WaveEngine/src/Vector3.h
@@ -4,26 +4,28 @@
 
 struct Vector2;
 
-WAVEEXPORT struct Vector3
+WAVEEXPORT struct Vector3 final
 {
-	float x = 0.0f;
-	float y = 0.0f;
-	float z = 0.0f;
+	float x;
+	float y;
+	float z;
 
 	static const float epsilon;
 
 	WAVEEXPORT Vector3();
 	WAVEEXPORT Vector3(const Vector2& vector2);
-	WAVEEXPORT Vector3(const float& x, const float& y);
-	WAVEEXPORT Vector3(const float& x, const float& y, const float& z);
+	WAVEEXPORT Vector3(const float& x, const float& y, const float& z = 0.0f);
 	WAVEEXPORT void ClampMagnitude(const float& maxLength);
 	WAVEEXPORT float Magnitude() const;
-	WAVEEXPORT float SqrtMagnitude() const;
+	WAVEEXPORT float SqrMagnitude() const;
 	WAVEEXPORT void Normalize();
+	WAVEEXPORT Vector3 Normalized() const;
+	WAVEEXPORT void MoveToWards(const Vector3& to, const float& distance);
 
 	WAVEEXPORT Vector3 operator/(const Vector3& other) const;
 	WAVEEXPORT Vector3 operator/(const float& scalar) const;
 	WAVEEXPORT friend Vector3 operator/(const float& scalar, const Vector3& other);
+	WAVEEXPORT void operator/=(const float& scalar);
 	WAVEEXPORT Vector3 operator+(const Vector3& other) const;
 	WAVEEXPORT void operator+=(const Vector3& other);
 	WAVEEXPORT Vector3 operator-(const Vector3& other) const;
@@ -31,20 +33,22 @@ WAVEEXPORT struct Vector3
 	WAVEEXPORT void operator=(const Vector2& vector2);
 	WAVEEXPORT Vector3 operator*(const float& scalar) const;
 	WAVEEXPORT friend Vector3 operator*(const float& scalar, const Vector3& vector3);
+	WAVEEXPORT void operator*=(const float& scalar);
 
 	WAVEEXPORT static Vector3 Up();
 	WAVEEXPORT static Vector3 Down();
 	WAVEEXPORT static Vector3 Right();
 	WAVEEXPORT static Vector3 Left();
 	WAVEEXPORT static Vector3 One();
-	WAVEEXPORT static Vector3 Front();
+	WAVEEXPORT static Vector3 Foward();
 	WAVEEXPORT static Vector3 Back();
+	WAVEEXPORT static Vector3 Zero();
 
 	WAVEEXPORT static float Angle(const Vector3& from, const Vector3& to);
 	WAVEEXPORT static Vector3 ClampMagnitude(const Vector3& vector, const float& maxLength);
 	WAVEEXPORT static Vector3 Cross(const Vector3& a, const Vector3& b);
 	WAVEEXPORT static float Magnitude(const Vector3& vector);
-	WAVEEXPORT static float SqrtMagnitude(const Vector3& vector);
+	WAVEEXPORT static float SqrMagnitude(const Vector3& vector);
 	WAVEEXPORT static float Dot(const Vector3& a, const Vector3& b);
 	WAVEEXPORT static Vector3 Lerp(const Vector3& a, const Vector3& b, float t);
 	WAVEEXPORT static Vector3 LerpUnclamp(const Vector3& a, const Vector3& b, const float& t);
@@ -53,4 +57,6 @@ WAVEEXPORT struct Vector3
 	WAVEEXPORT static Vector3 Min(const Vector3& a, const Vector3& b);
 	WAVEEXPORT static Vector3 Project(const Vector3& a, const Vector3& b);
 	WAVEEXPORT static Vector3 Normalized(const Vector3& a);
+	WAVEEXPORT static Vector3 MoveToWards(const Vector3& from, const Vector3& to, const float& distance);
+	WAVEEXPORT Vector3 Reflect(const Vector3& direction, const Vector3& normal);
 };
