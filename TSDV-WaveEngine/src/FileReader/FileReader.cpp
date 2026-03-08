d0425aff22cfc2f042ba74b9feea81f44d223298
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-08 04:03:33 -0300

    Improve FileReader
    Add the possibility to Save any type of class/structure

--- TSDV-WaveEngine/src/FileReader/FileReader.cpp
+++ TSDV-WaveEngine/src/FileReader/FileReader.cpp
@@ -16,17 +16,7 @@ namespace WaveEngine
 	{
 	}
 
-	string FileReader::GetAbsoluteFilePath(const string_view filePath) const
-	{
-		return filesystem::absolute(filePath).lexically_normal().string();
-	}
-
-	string FileReader::ReadFileAbsolutePath(const string_view filePath)
-	{
-		return ReadFile(GetAbsoluteFilePath(filePath));
-	}
-
-	string FileReader::ReadFile(const string_view filePath)
+	string FileReader::FileImporter(const string_view& filePath, const bool& binary)
 	{
 		ifstream inStream;
 
@@ -55,5 +45,30 @@ namespace WaveEngine
 			inStream.close();
 
 		return buffer.str();
+	}
+
+	string FileReader::GetAbsoluteFilePath(const string_view filePath) const
+	{
+		return filesystem::absolute(filePath).lexically_normal().string();
+	}
+
+	string FileReader::ReadFileAbsolutePath(const string_view filePath)
+	{
+		return ReadFile(GetAbsoluteFilePath(filePath));
+	}
+
+	string FileReader::ReadFile(const string_view filePath)
+	{
+		return FileImporter(filePath, false);
+	}
+
+	string FileReader::ReadFileBinary(const string_view filePath)
+	{
+		return FileImporter(filePath, true);
+	}
+
+	string FileReader::ReadFileBinaryAbsolutePath(const string_view filePath)
+	{
+		return ReadFileBinary(filePath);
 	}
 }
--- TSDV-WaveEngine/src/FileReader/FileReader.h
+++ TSDV-WaveEngine/src/FileReader/FileReader.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <fstream>
+#include <iostream>
+
 #include "ServiceProvider/Service.h"
 #include "Export.h"
 
@@ -20,6 +23,20 @@ namespace WaveEngine
 		FileReader();
 		~FileReader();
 
+		string FileImporter(const string_view& filePath, const bool& binary);
+
+		template<typename T>
+		void WriteIntoFile(ofstream& outStream, T& data)
+		{
+			outStream.write(reinterpret_cast<char*>(&data), sizeof(data));
+		}
+
+		template<typename T>
+		void LoadFromFile(ifstream& inStream, T& data)
+		{
+			inStream.read(reinterpret_cast<char*>(&data), sizeof(data));
+		}
+
 		friend class BaseGame;
 		friend class ServiceProvider;
 
@@ -27,5 +44,64 @@ namespace WaveEngine
 
 		WAVEEXPORT string ReadFileAbsolutePath(const string_view filePath);
 		WAVEEXPORT string ReadFile(const string_view filePath);
+
+		WAVEEXPORT string ReadFileBinary(const string_view filePath);
+		WAVEEXPORT string ReadFileBinaryAbsolutePath(const string_view filePath);
+
+		template<typename... T>
+		void SaveData(const string_view filePath, T&... data)
+		{
+			ofstream outStream;
+
+			try
+			{
+				outStream.open(filePath, ios::out | ios::binary);
+
+				if (!outStream.is_open())
+					throw runtime_error("FilePath; " + string(filePath) + " wasn't found.\n");
+
+				(WriteIntoFile(outStream, data), ...);
+
+			}
+			catch (const runtime_error& e)
+			{
+				cerr << e.what() << "\n";
+			}
+			catch (...)
+			{
+				cerr << "Unknow error detected while trying to read file.\n";
+			}
+
+			if (outStream.is_open())
+				outStream.close();
+		}
+
+		template<typename... T>
+		void LoadData(const string_view filePath, T&... data)
+		{
+			ifstream inStream;
+
+			try
+			{
+				inStream.open(filePath, ios::out | ios::binary);
+
+				if (!inStream.is_open())
+					throw runtime_error("FilePath; " + string(filePath) + " wasn't found.\n");
+
+				(LoadFromFile(inStream, data), ...);
+
+			}
+			catch (const runtime_error& e)
+			{
+				cerr << e.what() << "\n";
+			}
+			catch (...)
+			{
+				cerr << "Unknow error detected while trying to read file.\n";
+			}
+
+			if (inStream.is_open())
+				inStream.close();
+		}
 	};
 }
