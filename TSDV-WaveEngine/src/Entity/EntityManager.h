73abe353c7315615d391be0a257af34e48ae00eb
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-12 21:43:43 -0300

    Update EntityManager.h
    Move EntityChangeLayer to its own folder

--- TSDV-WaveEngine/src/Entity/EntityManager.h
+++ TSDV-WaveEngine/src/Entity/EntityManager.h
@@ -10,9 +10,8 @@
 #include <vector>
 
 #include "Entity/Entity.h"
+#include "Entity/Events/EntityChangeLayer.h"
 #include "Material/MaterialManager.h"
-
-#include "EventSystem/Event.h"
 
 class BaseGame;
 class EntityFactory;
@@ -26,31 +25,6 @@ namespace WaveEngine
 
 	template<typename T>
 	concept EntityManagerGetStandar = derived_from<T, Entity> || derived_from<T, Drawable>;
-
-	struct EntityChangeLayer : public Event
-	{
-		unsigned int ID = 0;
-		int oldLayer = 0;
-		int newLayer = 0;
-
-		EntityChangeLayer(const unsigned int& ID, const int& oldLayer, const int& newLayer)
-		{
-			this->ID = ID;
-			this->oldLayer = oldLayer;
-			this->newLayer = newLayer;
-		}
-
-		EntityChangeLayer()
-		{
-		}
-
-		void Reset() override
-		{
-			ID = 0;
-			oldLayer = 0;
-			newLayer = 0;
-		}
-	};
 
 	class EntityManager final : public Service
 	{
