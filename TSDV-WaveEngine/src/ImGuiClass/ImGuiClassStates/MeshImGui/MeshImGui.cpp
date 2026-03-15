288d663a41ba5fa8399e7be3253c1f40df586b64
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-15 01:50:55 -0300

    Update MeshImGui.cpp

--- TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MeshImGui/MeshImGui.cpp
+++ TSDV-WaveEngine/src/ImGuiClass/ImGuiClassStates/MeshImGui/MeshImGui.cpp
@@ -19,27 +19,27 @@ namespace WaveEngine
 
 	void MeshImGui::Update()
 	{
-		for (map<unsigned int, Drawable*>::iterator it = GetEntityManager()->GetDrawables().begin(); it != GetEntityManager()->GetDrawables().end(); ++it)
+		for (vector<Drawable*>::iterator it = GetEntityManager()->GetDrawables().begin(); it != GetEntityManager()->GetDrawables().end(); ++it)
 		{
-			text = "Entity ID: " + to_string(it->first);
+			text = "Entity ID: " + to_string((*it)->ID);
 			ImGui::Text(text.c_str());
 
-			for (int i = 0; i < it->second->vertexSize; ++i)
+			for (int i = 0; i < (*it)->vertexSize; ++i)
 			{
-				currentVertex = &it->second->vertex[i];
+				currentVertex = &(*it)->vertex[i];
 
-				text = to_string(i) + " Vertex Postion of ID: " + to_string(it->first);
+				text = to_string(i) + " Vertex Postion of ID: " + to_string((*it)->ID);
 				if (ImGui::DragFloat3(text.c_str(), &currentVertex->position.x))
-					it->second->UpdateVertexBuffer();
+					(*it)->UpdateVertexBuffer();
 
-				text = to_string(i) + "Vertex Color of ID: " + to_string(it->first);
+				text = to_string(i) + "Vertex Color of ID: " + to_string((*it)->ID);
 				if (ImGui::ColorEdit4(text.c_str(), &currentVertex->color.x))
-					it->second->UpdateVertexBuffer();
+					(*it)->UpdateVertexBuffer();
 
 
-				text = to_string(i) + "Vertex UV: " + to_string(it->first);
+				text = to_string(i) + "Vertex UV: " + to_string((*it)->ID);
 				if (ImGui::DragFloat2(text.c_str(), &currentVertex->textureCordinates.x))
-					it->second->UpdateVertexBuffer();
+					(*it)->UpdateVertexBuffer();
 
 				ImGui::Separator();
 
